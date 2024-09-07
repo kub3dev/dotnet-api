2ea1e53a1dbed2f244ba15f9b933a8caff535e3c
Cleber Gomes <cleber@gomes.dev>
2024-09-07 13:40:39 -0300

    fix: ajustes

--- Voucher.API/Program.cs
+++ Voucher.API/Program.cs
@@ -1,4 +1,5 @@
 using Keycloak.AuthServices.Authentication;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,31 @@ builder.Services.AddAuthorization();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Voucher | API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+             new OpenApiSecurityScheme {
+                 Reference = new OpenApiReference {
+                     Type = ReferenceType.SecurityScheme,
+                         Id = "Bearer"
+                 }
+             },
+            new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -22,7 +47,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 var summaries = new[]
 {
